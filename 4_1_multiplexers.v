244f0f361be3be4f4a8e7515b7607de8a35a9f62
oud3noida <51070450+oud3noida@users.noreply.github.com>
2019-12-07 20:47:09 +0900

    reg -> wire

--- 4_1_multiplexers.v
+++ 4_1_multiplexers.v
@@ -1,10 +1,11 @@
 module multiplexer_4_1(a,b,i_0,i_1,i_2,i_3,o);
   input a,b;
   input [3:0] i_0,i_1,i_2,i_3;
-  output reg [3:0] o;
-  reg [3:0] t1,t2;
+  output [3:0] o;
+  wire [3:0] t1,t2;
 
-  multiplexer_2_1_reg first(b,i_0,i_1,t1);
-  multiplexer_2_1_reg second(b,i_2,i_3,t2);
-  multiplexer_2_1_reg third(a,t1,t2,o);
+  multiplexer_2_1_4bit first(b,i_0,i_1,t1);
+  multiplexer_2_1_4bit second(b,i_2,i_3,t2);
+  multiplexer_2_1_4bit third(a,t1,t2,o);
+
 endmodule
