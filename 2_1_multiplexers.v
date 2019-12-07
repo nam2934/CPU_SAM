2a08a84e969bf1e84009fb32c7e847d43987a9fb
oud3noida <51070450+oud3noida@users.noreply.github.com>
2019-12-07 20:15:51 +0900

    reg ver added

--- 2_1_multiplexers.v
+++ 2_1_multiplexers.v
@@ -7,3 +7,16 @@ module multiplexer_2_1(a,i_0,i_1,o);
   assign t2 = a & i_1;
   assign o = t1 | t2;
 endmodule
+
+module multiplexer_2_1_reg(
+  input a,
+  input i_0,
+  input i_1,
+  output reg o
+);
+  reg t1, t2;
+  
+  t1 = ~a & i_0;
+  t2 = a & i_1;
+  o = t1 | t2;
+endmodule
