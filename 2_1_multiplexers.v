46c3d363f702b1f6f7e4ebf356736b801d39b5a3
oud3noida <51070450+oud3noida@users.noreply.github.com>
2019-12-07 21:02:27 +0900

    Update 2_1_multiplexers.v

--- 2_1_multiplexers.v
+++ 2_1_multiplexers.v
@@ -12,7 +12,12 @@ module multiplexer_2_1_4bit(a,i_0,i_1,o);
   input a;
   input [3:0] i_0,i_1;
   output [3:0] o;
-  wire [3:0] t1,t2;
+  wire [3:0] a_, t1,t2;
+  
+  assign a_[0] = a;
+  assign a_[1] = a;
+  assign a_[2] = a;
+  assign a_[3] = a;
 
   assign t1 = ~a & i_0;
   assign t2 = a & i_1;
