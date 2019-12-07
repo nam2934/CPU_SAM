ea5be09fe625e4c22db531135cb3202d834b48a0
oud3noida <51070450+oud3noida@users.noreply.github.com>
2019-12-07 20:53:57 +0900

    정남진 머가리박아

--- 4_1_multiplexers.v
+++ 4_1_multiplexers.v
@@ -4,8 +4,8 @@ module multiplexer_4_1(a,b,i_0,i_1,i_2,i_3,o);
   output [3:0] o;
   wire [3:0] t1,t2;
 
-  multiplexer_2_1_4bit first(b,i_0,i_1,t1);
-  multiplexer_2_1_4bit second(b,i_2,i_3,t2);
-  multiplexer_2_1_4bit third(a,t1,t2,o);
+  multiplexer_2_1_4bit first(a,i_0,i_1,t1);
+  multiplexer_2_1_4bit second(a,i_2,i_3,t2);
+  multiplexer_2_1_4bit third(b,t1,t2,o);
 
 endmodule
