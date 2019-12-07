8f8575dd9d9389a5c0b4a26a44558faa57c99e09
oud3noida <51070450+oud3noida@users.noreply.github.com>
2019-12-07 20:24:38 +0900

    Minor update

--- 4_1_multiplexers.v
+++ 4_1_multiplexers.v
@@ -4,7 +4,7 @@ module multiplexer_4_1(a,b,i_0,i_1,i_2,i_3,o);
   output reg [3:0] o;
   reg [3:0] t1,t2;
 
-  multiplexer_2_1 first(b,i_0,i_1,t1);
-  multiplexer_2_1 second(b,i_2,i_3,t2);
-  multiplexer_2_1 third(a,t1,t2,o);
+  multiplexer_2_1_reg first(b,i_0,i_1,t1);
+  multiplexer_2_1_reg second(b,i_2,i_3,t2);
+  multiplexer_2_1_reg third(a,t1,t2,o);
 endmodule
