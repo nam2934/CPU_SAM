4ff589275fad6b03afd532e787762ab8ceca7f50
oud3noida <51070450+oud3noida@users.noreply.github.com>
2019-12-07 20:46:51 +0900

    4-bit reg ver to 4-bit wire ver

--- 2_1_multiplexers.v
+++ 2_1_multiplexers.v
@@ -8,17 +8,13 @@ module multiplexer_2_1(a,i_0,i_1,o);
   assign o = t1 | t2;
 endmodule
 
-module multiplexer_2_1_reg(
-  input a,
-  input [3:0] i_0,
-  input [3:0] i_1,
-  output reg [3:0] o
-);
-  reg [3:0] t1, t2;
-  
-  always begin
-    t1 = ~a & i_0;
-    t2 = a & i_1;
-    o = t1 | t2;
-  end
+module multiplexer_2_1_4bit(a,i_0,i_1,o);
+  input a;
+  input [3:0] i_0,i_1;
+  output [3:0] o;
+  wire [3:0] t1,t2;
+
+  assign t1 = ~a & i_0;
+  assign t2 = a & i_1;
+  assign o = t1 | t2;
 endmodule
